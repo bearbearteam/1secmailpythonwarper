e5ebd8d47bffda8251f804d9c5644f79275dc07e
bearbearteam <77270434+bearbearteam@users.noreply.github.com>
2022-11-01 19:00:35 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+   name='1secmail python',
+   version='0.1',
+   description='A useful module for accessing the api of 1secmail',
+   author='pfp',
+   packages=['1secmail'],  
+   install_requires=['requests'], #needs requests 
+
